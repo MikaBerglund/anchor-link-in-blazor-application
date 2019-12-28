98a75bff90fbdccaf0103e4f6a6fafee957fab64
Mika Berglund <mika.berglund@meteoriitti.com>
2019-12-28 09:42:34 +0200

    Added AnchorLink component and a page that demonstrates how it works.

---
+++ BlazorWasmApplication/wwwroot/js/anchorLink.js
@@ -0,0 +1,16 @@
+﻿
+window.anchorLink = {
+    init: function (anchorId) {
+        var elem = document.getElementById(anchorId);
+        if (elem) {
+            elem.setAttribute("onclick", "return false;");
+        }
+    },
+    scrollIntoView: function (elementId) {
+        var elem = document.getElementById(elementId);
+        if (elem) {
+            elem.scrollIntoView();
+            window.location.hash = elementId;
+        }
+    }
+}
