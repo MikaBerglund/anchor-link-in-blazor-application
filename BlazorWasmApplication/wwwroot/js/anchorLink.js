754f3cf8d1f2aedb46be28c145c836b8d9dbeda9
Mika Berglund <mika.berglund@meteoriitti.com>
2019-12-28 11:59:20 +0200

    As it turns out, Razor components now have support for preventing the default action when for instance clicking on an element. The AnchorLink is modified to leverage this support, which simplifies the component a lot. It also now works as a standard link too, depending on if the href attribute starts with '#' or not.

--- BlazorWasmApplication/wwwroot/js/anchorLink.js
+++ BlazorWasmApplication/wwwroot/js/anchorLink.js
@@ -1,11 +1,5 @@
 ﻿
 window.anchorLink = {
-    init: function (anchorId) {
-        var elem = document.getElementById(anchorId);
-        if (elem) {
-            elem.setAttribute("onclick", "return false;");
-        }
-    },
     scrollIntoView: function (elementId) {
         var elem = document.getElementById(elementId);
         if (elem) {
